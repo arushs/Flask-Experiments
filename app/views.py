a3a1ed838177e9f6601ba3a2e575c507204219fd
arushs <arushshankar@gmail.com>
2014-05-25 23:25:43 -0700

    Added more templating for fake users

--- app/views.py
+++ app/views.py
@@ -5,4 +5,14 @@ from flask import render_template
 @app.route('/index')
 def index():
     user = { 'nickname': 'Arush' } # fake user
-    return render_template("index.html", title = 'Home', user = user)
+    posts = [ # fake array of posts
+              {
+                  'author': { 'nickname': 'John' },
+                  'body': 'Beautiful day in Portland!'
+              },
+              {
+                  'author': { 'nickname': 'Susan' },
+                  'body': 'The Avengers movie was so cool!'
+              }
+    ]
+    return render_template("index.html", title = "Home", user=user, posts=posts)
