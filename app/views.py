ef0d6c0d81ec89607d0e6503ad0701909a8abef9
arushs <arushshankar@gmail.com>
2014-05-25 20:28:42 -0700

    Added templating

--- app/views.py
+++ app/views.py
@@ -1,16 +1,8 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = { 'nickname': 'Arush' } # fake user
-    return '''
-<html>
-  <head>
-    <title>Home Page</title>
-  </head>
-  <body>
-    <h1>Hello, ''' + user['nickname'] + '''</h1>
-  </body>
-</html>
-'''
+    return render_template("index.html", title = 'Home', user = user)
